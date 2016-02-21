b2dc076648cdea13716322be8347ceb2a99d660b
Anko Anchev <anko.anchev@gmail.com>
2016-02-21 15:07:06 +0000

    Added a Floor tile and managed Tags
    + A SpriteRandomizer is used to create variaty

---
+++ Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpriteRandomizer : MonoBehaviour {
+    public List<Sprite> sprites;
+
+    void Start ()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            Sprite sprite = sprites[Random.Range(0, sprites.Count)];
+            spriteRenderer.sprite = sprite;
+        }
+	}
+}
