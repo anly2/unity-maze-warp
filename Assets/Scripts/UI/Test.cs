02efbfdd675dcdf7f8a4b23c3e13e14387757012
Sofakinglazy <yx5m14@soton.ac.uk>
2016-02-24 18:13:38 +0000

    Finished Time Count and Status Display

---
+++ Assets/Scripts/UI/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public static Test instance = null;
+
+    public GameObject HUD;
+
+    UIManager uiManager;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        uiManager = HUD.GetComponent<UIManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            uiManager.GameStart();
+    }
+}
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float restartDelay = 2f;
+
+    public Image screenCover;
+    public Text levelText;
+    public Text deathText;
+    public Text turnText;
+    public Text timeText;
+
+    void Awake()
+    {
+    }
+
+    void Update()
+    {
+        timeText.text = "Time: " + (int) TimeTracker.timer;
+        Debug.Log(TimeTracker.timer);
+        deathText.text = "Death: " + 0;
+        turnText.text = "Turn: " + 0 + "/" + 10;
+        //deathText.text = "Death: " + GameController.instance.GetDeathCount();
+        //turnText.text = "Turn: " + GameController.instance.GetTurnCount();
+    }
+
+    public void UpdateLevelInfo(int level)
+    {
+        levelText.text = "Level: " + level;
+    }
+
+    public void UpdateDeathCount(int death)
+    {
+        deathText.text = "Death: " + death;
+    }
+
+    public void UpdateTurnCount(int leftTurn, int totalTurns)
+    {
+        turnText.text = "Turn: " + leftTurn + "/" + totalTurns;
+    }
+
+    public void GameStart()
+    {
+        deathText.enabled = false;
+        turnText.enabled = false;
+        timeText.enabled = false;
+        screenCover.enabled = true;
+        levelText.enabled = true;
+        StartCoroutine(RestartLevel());
+    }
+
+    public void GameOver()
+    {
+        screenCover.enabled = true;
+        //levelText.enabled = true;
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(restartDelay);
+
+        Application.LoadLevel(Application.loadedLevel);
+        screenCover.enabled = false;
+        //levelText.enabled = false;
+    }
 }
