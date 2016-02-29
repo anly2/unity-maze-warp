657a775deff2ebd842ef8758bad8a963507e8d47
Anko Anchev <anko.anchev@gmail.com>
2016-02-29 14:45:43 +0000

    Removed unused CameraExt methods (potentially confusing)

--- Assets/Scripts/Utilities/CameraExtensions.cs
+++ Assets/Scripts/Utilities/CameraExtensions.cs
@@ -3,25 +3,12 @@ using System.Collections;
 
 public static class CameraExtensions
 {
-    public static float DEFAULT_MOTION_DURATION = 1f;
-    
-    
-    public static IEnumerator MotionTo(this Camera camera, Vector2 target)
-    {
-        return camera.MotionTo(target, DEFAULT_MOTION_DURATION);
-    }
-
     public static IEnumerator MotionTo(this Camera camera, Vector2 target, float duration)
     {
         Vector3 end = new Vector3(target.x, target.y, camera.transform.position.z);
         return camera.MotionTo(end, duration);
     }
-
-    public static IEnumerator MotionTo(this Camera camera, Vector3 target)
-    {
-        return camera.MotionTo(target, DEFAULT_MOTION_DURATION);
-    }
-
+    
     public static IEnumerator MotionTo(this Camera camera, Vector3 target, float duration)
     {
         Vector3 start = camera.transform.position;
