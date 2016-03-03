e73803ac73349efd353b68bbe36ecd7c108448ed
Sofakinglazy <yx5m14@soton.ac.uk>
2016-03-03 15:24:10 +0000

    Added tips and relative images

--- Assets/Scripts/Items/DirectionSign.cs
+++ Assets/Scripts/Items/DirectionSign.cs
@@ -6,31 +6,40 @@ public class DirectionSign : MonoBehaviour {
     public GameObject signObject;
 
     private Transform player;
-    private bool firstTime = true;
+    private bool firstTime;
     private GameObject signClone = null;
 
-    void Awake()
+    void Start()
     {
+        firstTime = true;
     }
 
     void Update()
     {
         player = GameObject.Find("Player").transform;
 
-        if (signObject == null )
+        if (signObject == null)
             return;
 
-        if (firstTime)
-        {
-            Vector3 signPos = new Vector3(player.position.x, player.position.y - 1, player.position.z);
-            signClone = Instantiate(signObject, signPos, Quaternion.identity) as GameObject;
-            firstTime = false;
-        }
+        StartCoroutine(InstantiateSign());
 
         if (signClone != null && player.position == signClone.transform.position)
             Destroy(signClone);
    
     }
 
+    IEnumerator InstantiateSign()
+    {
+        if (firstTime)
+        {
+            Vector3 signPos = new Vector3(player.position.x, player.position.y - 1, player.position.z);
+            signClone = Instantiate(signObject, signPos, Quaternion.identity) as GameObject;
+            firstTime = false;
+        }
+        else
+        {
+            yield return null;
+        }
 
+    }
 }
